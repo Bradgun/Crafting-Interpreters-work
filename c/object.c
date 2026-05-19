314dda1d845d593faeb2b425fffa4066a6aa378f
Bradgun <72219553+Bradgun@users.noreply.github.com>
2026-05-19 13:51:10 -0700

    Complete chapter 19 challenge 2

--- c/object.c
+++ c/object.c
@@ -19,28 +19,19 @@ static Obj* allocateObject(size_t size, ObjType type) {
   return object;
 }
 
-static ObjString* allocateString(char* chars, int length) {
+ObjString* makeString(bool ownsChars, char* chars, int length) {
   ObjString* string = ALLOCATE_OBJ(ObjString, OBJ_STRING);
+  string->ownsChars = ownsChars;
   string->length = length;
   string->chars = chars;
+
   return string;
-}
-
-ObjString* takeString(char* chars, int length) {
-  return allocateString(chars, length);
-}
-
-ObjString* copyString(const char* chars, int length) {
-  char* heapChars = ALLOCATE(char, length + 1);
-  memcpy(heapChars, chars, length);
-  heapChars[length] = '\0';
-  return allocateString(heapChars, length);
 }
 
 void printObject(Value value) {
   switch (OBJ_TYPE(value)) {
     case OBJ_STRING:
-      printf("%s", AS_CSTRING(value));
+      printf("%.*s", AS_STRING(value)->length, AS_CSTRING(value));
       break;
   }
 }
